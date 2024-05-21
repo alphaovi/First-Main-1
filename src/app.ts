4120187803aea36486bbcd6e08d92fe2d9f14e4d
alphaovi <ovi778811@gmail.com>
2024-05-22 02:37:41 +0600

    after solve every single issue

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 const app: Application = express();
-// const port = 3000;
+// const port = process.env.PORT || 5000;
+const port = 3000;
 
 // parser
 app.use(express.json());
