3fdfc2f56a8f0f735c6fab2f3ef86d840d4efcaf
alphaovi <ovi778811@gmail.com>
2024-05-23 00:35:48 +0600

    after testing the apis

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentsRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 // const port = process.env.PORT || 5000;
 const port = 3000;
@@ -8,9 +9,15 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+// application routes
+
+app.use('/api/v1/students', StudentsRoutes);
+
+const getAController = (req: Request, res: Response) => {
   res.send('Hello World!');
-});
+};
+
+app.get('/', getAController);
 
 console.log(process.cwd());
 
---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const student = req.body.student;
+
+    // will call service function to this data
+    const result = await StudentServices.createStudentIntoDB(student);
+
+    // send response
+    res.status(200).json({
+      success: true,
+      message: 'Student is created successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+    res.status(200).json({
+      success: true,
+      message: 'Students are retrieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleStudent = async(req: Request, res: Response) => {
+  try{
+    const studentId = req.params.studentid;
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+    res.status(200).json({
+      success:true,
+      message: "We got the single student",
+      data: result
+    })
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
